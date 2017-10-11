3b35f1cfe726990e24c116e63d518bbc27c4ae19
Dante Nardo <dante.nardo@outlook.com>
2017-10-10 22:54:00 -0400

    A02 - 3D Primitives

--- A02 - 3D Primitives/AppClass.cpp
+++ A02 - 3D Primitives/AppClass.cpp
@@ -21,13 +21,13 @@ void Application::InitVariables(void)
 	m_pTube = new MyMesh();
 	m_pTube->GenerateTube(0.5f, 0.35f, 1.0f, 7, C_GREEN);
 	m_pSphere = new MyMesh();
-	m_pSphere->GenerateSphere(0.5f, 3, C_BLUE);
+	m_pSphere->GenerateSphere(0.5f, 7, C_BLUE);
 	m_pTorus = new MyMesh();
 	m_pTorus->GenerateTorus(0.5f, 0.30f, 7, 7, C_VIOLET);
 
 	//Make Mesh object
 	m_pMesh = new MyMesh();
-	int primitive = 0;
+	int primitive = 1;
 	switch (primitive)
 	{
 	default:
--- A02 - 3D Primitives/MyMesh.cpp
+++ A02 - 3D Primitives/MyMesh.cpp
@@ -275,8 +275,55 @@ void MyMesh::GenerateCone(float a_fRadius, float a_fHeight, int a_nSubdivisions,
 	Release();
 	Init();
 
-	// Replace this with your code
-	GenerateCube(a_fRadius * 2.0f, a_v3Color);
+	// =============================================================================
+	// =============================== MY CONE CODE ================================
+	// =============================================================================
+	#pragma region ConeCode
+
+	//				WHAT ARE CONE
+	// A cone is a circle with multiple points on it
+	// that all converge at a certain height with an
+	// x position equal to the center of the circle
+
+	//				HOW MAKE CONE
+	// Generate an amount of points equal to 360
+	// divided by amount of subdivisions and then
+	// connect them to one center point
+
+	//			    CREATE CIRCLE
+	// Set starting degree angle and then continue
+	// creating points by rotating until 360
+	std::vector<vector3> points = std::vector<vector3>();
+	int angle = 360 / a_nSubdivisions;
+	int cAngle = 0;
+	while (cAngle < 360) {
+		points.push_back(vector3(a_fRadius * cos(cAngle * PI / 180), 0,
+								 a_fRadius * sin(cAngle * PI / 180)));
+		cAngle += angle;
+	}
+
+	//			 DEFINE CENTER POINTS
+	// Set top point at center of circle and equal to
+	// the height value given as a param and bottom at 0
+	vector3 bottom = vector3(0, 0, 0);
+	vector3 top = vector3(0, a_fHeight, 0);
+
+	//				CREATE TRIS
+	// Iterate through points and create a tri every
+	// other point from two circle points to top
+	int next = 0;
+	for (int i = 0; i < points.size(); i++) {
+
+		// Determine next index
+		next = (i + 1 >= points.size()) ? 0 : i + 1;
+
+		// Connect tris to bottom
+		AddTri(points[next], bottom, points[i]);
+		
+		// Connect tris to top
+		AddTri(points[i], top, points[next]);
+	}
+	#pragma endregion
 	// -------------------------------
 
 	// Adding information about color
@@ -299,8 +346,57 @@ void MyMesh::GenerateCylinder(float a_fRadius, float a_fHeight, int a_nSubdivisi
 	Release();
 	Init();
 
-	// Replace this with your code
-	GenerateCube(a_fRadius * 2.0f, a_v3Color);
+	// =============================================================================
+	// ============================ MY CYLINDER CODE ===============================
+	// =============================================================================
+	#pragma region CylinderCode
+
+	//			  WHAT ARE CYLINDER
+	// A cylinder is two circles that connect each
+	// point vertically to a point on the other circle.
+
+	//			  HOW MAKE CYLINDER
+	// Generate a circle with a bottom face, and a
+	// circle with a top face. Connect every vertical
+	// point on the circle.
+
+	//			 CREATE BOTH CIRCLES
+	// Set starting degree angle and then continue
+	// creating points by rotating until 360
+	std::vector<vector3> bCircle = std::vector<vector3>();
+	std::vector<vector3> tCircle = std::vector<vector3>();
+	int angle = 360 / a_nSubdivisions;
+	int cAngle = 0;
+	while (cAngle < 360) {
+		bCircle.push_back(vector3(a_fRadius * cos(cAngle * PI / 180), 0,
+								  a_fRadius * sin(cAngle * PI / 180)));
+		tCircle.push_back(vector3(a_fRadius * cos(cAngle * PI / 180), a_fHeight,
+								  a_fRadius * sin(cAngle * PI / 180)));
+		cAngle += angle;
+	}
+
+	//			 CREATE BOTH CENTERS
+	// Bottom face and top face both need a center point
+	vector3 bCenter = vector3(0, 0, 0);
+	vector3 tCenter = vector3(0, a_fHeight, 0);
+
+	//		  CREATE TRI AND QUAD FACES
+	// Set starting degree angle and then continue
+	// creating points by rotating until 360
+	int next = 0;
+	for (int i = 0; i < bCircle.size(); i++) {
+
+		// Determine next index
+		next = (i + 1 >= bCircle.size()) ? 0 : i + 1;
+
+		// Create bottom and top faces
+		AddTri(bCircle[next], bCenter, bCircle[i]);
+		AddTri(tCircle[i], tCenter, tCircle[next]);
+
+		// Connect bottom circle to top circle
+		AddQuad(bCircle[next], bCircle[i], tCircle[next], tCircle[i]);
+	}
+	#pragma endregion
 	// -------------------------------
 
 	// Adding information about color
@@ -329,8 +425,64 @@ void MyMesh::GenerateTube(float a_fOuterRadius, float a_fInnerRadius, float a_fH
 	Release();
 	Init();
 
-	// Replace this with your code
-	GenerateCube(a_fOuterRadius * 2.0f, a_v3Color);
+	// =============================================================================
+	// ============================== MY TUBE CODE =================================
+	// =============================================================================
+	#pragma region TubeCode
+
+	//			  WHAT ARE TUBE
+	// A tube is two cylinders where a smaller cylinder
+	// "cuts out" a portion of a larger cylinder
+
+	//			  HOW MAKE CYLINDER
+	// Generate two bottom circles, one with smaller radius.
+	// Generate two top circles, one with smaller radius.
+	// Create quad walls between both inner circles.
+	// Create quad walls between both outer circles.
+
+	//			 CREATE ALL CIRCLES
+	// Set starting degree angle and then continue
+	// creating points by rotating until 360
+	std::vector<vector3> bICircle = std::vector<vector3>(); // bottom inner circle
+	std::vector<vector3> tICircle = std::vector<vector3>(); // top    inner circle
+	std::vector<vector3> bOCircle = std::vector<vector3>(); // bottom outer circle
+	std::vector<vector3> tOCircle = std::vector<vector3>(); // top    outer circle
+	int angle = 360 / a_nSubdivisions;
+	int cAngle = 0;
+	while (cAngle < 360) {
+		bICircle.push_back(vector3(a_fInnerRadius * cos(cAngle * PI / 180), 0,
+								   a_fInnerRadius * sin(cAngle * PI / 180)));
+		bOCircle.push_back(vector3(a_fOuterRadius * cos(cAngle * PI / 180), 0,
+								   a_fOuterRadius * sin(cAngle * PI / 180)));
+		tICircle.push_back(vector3(a_fInnerRadius * cos(cAngle * PI / 180), a_fHeight,
+								   a_fInnerRadius * sin(cAngle * PI / 180)));
+		tOCircle.push_back(vector3(a_fOuterRadius * cos(cAngle * PI / 180), a_fHeight,
+								   a_fOuterRadius * sin(cAngle * PI / 180)));
+		cAngle += angle;
+	}
+
+	//		  CREATE TRI AND QUAD FACES
+	// Set starting degree angle and then continue
+	// creating points by rotating until 360
+	int next = 0;
+	for (int i = 0; i < bICircle.size(); i++) {
+
+		// Determine next index
+		next = (i + 1 >= bICircle.size()) ? 0 : i + 1;
+
+		// Connect bottom inner and bottom outer circles
+		AddQuad(bICircle[next], bICircle[i], bOCircle[next], bOCircle[i]);
+
+		// Connect top inner and top outer circles
+		AddQuad(tICircle[i], tICircle[next], tOCircle[i], tOCircle[next]);
+
+		// Connect bottom inner circle to top inner circle
+		AddQuad(bICircle[i], bICircle[next], tICircle[i], tICircle[next]);
+
+		// Connect bottom outer circle to top outer circle
+		AddQuad(bOCircle[next], bOCircle[i], tOCircle[next], tOCircle[i]);
+	}
+	#pragma endregion
 	// -------------------------------
 
 	// Adding information about color
@@ -361,7 +513,10 @@ void MyMesh::GenerateTorus(float a_fOuterRadius, float a_fInnerRadius, int a_nSu
 	Release();
 	Init();
 
-	// Replace this with your code
+	// =============================================================================
+	// ============================== MY TORUS CODE ================================
+	// =============================================================================
+
 	GenerateCube(a_fOuterRadius * 2.0f, a_v3Color);
 	// -------------------------------
 
@@ -386,8 +541,135 @@ void MyMesh::GenerateSphere(float a_fRadius, int a_nSubdivisions, vector3 a_v3Co
 	Release();
 	Init();
 
-	// Replace this with your code
-	GenerateCube(a_fRadius * 2.0f, a_v3Color);
+	// =============================================================================
+	// ============================== MY SPHERE CODE ===============================
+	// =============================================================================
+	#pragma region SphereCode
+
+	//			  WHAT ARE SPHERE
+	// A sphere is a bunch of layered circles with 
+
+	//			  HOW MAKE SPHERE
+	// Generate a bunch of horizontal circles whose 
+	// radius and y position are determined by a
+	// vertical circle that we "create" by keeping
+	// track of an incremental representation of the
+	// angles of the points on that vertical circle.
+	// Then we make two bottom faces similar to how
+	// we did it with a cone.
+
+	//			 CREATE ALL CIRCLES
+	// Set starting degree angle and then continue
+	// creating points by rotating until 360
+	std::vector<std::vector<vector3>> circles = std::vector<std::vector<vector3>>();
+	float radius = 1.0f;
+	float lower = -0.1f;
+	float higher = 0.1f;
+
+	// rAngle and crAngle are used to determine the height
+	// positions of the numerous circle bands
+	int rAngle = 90 / (a_nSubdivisions);
+	int crAngle = 0;
+
+	// Set initial radius, lower, and higher values
+	radius  = cos(crAngle * PI / 180) * a_fRadius;
+	lower = -(sin(crAngle * PI / 180) * a_fRadius);
+	higher  = sin(crAngle * PI / 180) * a_fRadius;
+
+	// Create an amount of circles equal to subdivisions
+	for (int i = 0; i < a_nSubdivisions - 1; i++) {
+
+		// Generate two circles
+		std::vector<vector3> lCircle = std::vector<vector3>(); // lower circle
+		std::vector<vector3> hCircle = std::vector<vector3>(); // higher circle
+		int angle = 360 / a_nSubdivisions;
+		int cAngle = 0;
+		while (cAngle < 360) {
+			vector3 lPoint = vector3(radius * cos(cAngle * PI / 180), lower,
+								     radius * sin(cAngle * PI / 180));
+			vector3 hPoint = vector3(radius * cos(cAngle * PI / 180), higher,
+								     radius * sin(cAngle * PI / 180));
+			lCircle.push_back(lPoint);
+			hCircle.push_back(hPoint);
+			cAngle += angle;
+		}
+
+		// Radius is determined by width (thus cos)
+		// Lower and higher are determined by height (thus sin)
+		crAngle += rAngle;
+		radius  = cos(crAngle * PI / 180) * a_fRadius;
+		lower = -(sin(crAngle * PI / 180) * a_fRadius);
+		higher  = sin(crAngle * PI / 180) * a_fRadius;
+		circles.push_back(lCircle);
+		circles.push_back(hCircle);
+	}
+
+	//		  CREATE TRI AND QUAD FACES
+	// Create the center band of the sphere first
+	// then alternate between upper and lower bands.
+	// Afterwards, create bottom and then top faces.
+	int nextJ = 0;
+	int nextK = 0;
+	bool even = true;
+	for (int j = 0; j < circles.size(); j++) {
+		for (int k = 0; k < circles[j].size(); k++) {
+
+			// Create middle odd circle band and then branch out
+			if (j == 0) {
+
+				// Determine next indexes
+				nextJ = j + 1;
+				nextK = (k + 1 >= circles[j].size()) ? 0 : k + 1;
+				AddQuad(circles[j][k], circles[j][nextK], circles[j + 1][k], circles[j + 1][nextK]);
+				AddQuad(circles[j][k], circles[j][nextK], circles[j + 2][k], circles[j + 2][nextK]);
+			}
+
+			// Create every other band
+			else {
+
+				// Determine next indexes
+				nextJ = (j + 2 >= circles.size()) ? 0 : j + 2;
+				nextK = (k + 1 >= circles[j].size()) ? 0 : k + 1;
+
+				if (even) AddQuad(circles[nextJ][nextK], circles[nextJ][k], circles[j][nextK], circles[j][k]);
+				else	  AddQuad(circles[j][nextK], circles[j][k], circles[nextJ][nextK], circles[nextJ][k]);
+			}
+		}
+
+		// Boolean to easily keep track of true or false
+		// to draw quads facing the right way
+		even = (even == false) ? true : false;
+	}
+
+	//		CREATE BOTTOM AND TOP FACES
+	// Select the last two circles and create tris
+	// to the two generated points
+	crAngle += rAngle;
+	radius  = cos(crAngle * PI / 180) * a_fRadius;
+	lower = -(sin(crAngle * PI / 180) * a_fRadius);
+	higher  = sin(crAngle * PI / 180) * a_fRadius;
+	vector3 bottom = vector3(radius * cos(crAngle * PI / 180), lower,
+							 radius * sin(crAngle * PI / 180));
+	vector3 top =	 vector3(radius * cos(crAngle * PI / 180), higher,
+							 radius * sin(crAngle * PI / 180));
+
+	// Iterate through the last two circles and create bottom and top faces
+	// The first loop is the bottom
+	int next = 0;
+	int index = circles.size() - 1;
+	for (int l = 0; l < circles[index].size(); l++) {
+		next = (l + 1 >= circles[index].size()) ? 0 : l + 1;
+		AddTri(circles[index][next], circles[index][l], top);
+	}
+	// The second loop is the top
+	next = 0;
+	index--;
+	for (int m = 0; m < circles[index].size(); m++) {
+		next = (m + 1 >= circles[index].size()) ? 0 : m + 1;
+		AddTri(circles[index][m], circles[index][next], bottom);
+	}
+
+	#pragma endregion
 	// -------------------------------
 
 	// Adding information about color
